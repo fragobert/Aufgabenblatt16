2b46a3d5363f8d05a9df19a3fa3cb4021de75f19
Florian Stafler <ststaflo@bx.fallmerayer.it>
2023-05-17 22:24:19 +0200

    gui for client Chat

--- src/main/java/aufgabe16/aufgabenblatt16/ChatClient2.java
+++ src/main/java/aufgabe16/aufgabenblatt16/ChatClient2.java
@@ -1,14 +1,69 @@
 package aufgabe16.aufgabenblatt16;
 
-import java.io.*;
-import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ChatClient2 {
+    private JFrame frame;
+    private JTextField messageField;
+    private JTextArea chatArea;
+    private JButton sendButton;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
     private String username;
     private String commandPrefix = "/";
+
+    public ChatClient2() {
+        createGUI();
+    }
+
+    private void createGUI() {
+        frame = new JFrame("Chat Client");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        frame.add(new JScrollPane(chatArea), BorderLayout.CENTER);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        messageField = new JTextField();
+        bottomPanel.add(messageField, BorderLayout.CENTER);
+
+        messageField.addActionListener(e -> {
+            sendMessage();
+        });
+
+        sendButton = new JButton("Send");
+        bottomPanel.add(sendButton, BorderLayout.EAST);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+
+        sendButton.addActionListener(e -> {
+            sendMessage();
+        });
+
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+    public void sendMessage(){
+        String message = messageField.getText();
+        if(!message.isEmpty()) {
+            if (message.startsWith(commandPrefix)) {
+                handleCommand(message.substring(1));
+            } else {
+                chatArea.append("Ich: " + message + "\n");
+                out.println(username + ": " + message);
+            }
+            messageField.setText("");
+        }
+    }
 
     public void start(String serverIp, int port, String username) {
         this.username = username;
@@ -17,69 +72,46 @@ public class ChatClient2 {
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            // Senden des Benutzernamens an den Server
             out.println(username);
 
-            // Erstellen eines Threads, um die eingehenden Nachrichten zu lesen und anzuzeigen
             Thread messageReaderThread = new Thread(() -> {
                 try {
                     String serverResponse;
                     while ((serverResponse = in.readLine()) != null) {
                         if (serverResponse.startsWith("!")) {
-                            handleCommand(serverResponse); // Behandlung von Befehlen
+                            handleCommand(serverResponse);
                         } else {
-                            System.out.println(serverResponse);
+                            chatArea.append(serverResponse + "\n");
                         }
                     }
-                } catch (IOException e) {
-                    System.err.println("Error reading server response: " + e.getMessage());
+                } catch (Exception e) {
+                    chatArea.append("Error reading server response: " + e.getMessage() + "\n");
                 }
             });
             messageReaderThread.start();
-
-            // Lesen der Benutzereingaben und Senden an den Server
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                if (userInput.startsWith(commandPrefix)) {
-                    handleCommand(userInput.substring(1)); // Behandlung von Befehlen
-                } else {
-                    out.println(username + ": " + userInput);
-                }
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + serverIp);
+        } catch (Exception e) {
+            chatArea.append("Couldn't get I/O for the connection to " + serverIp + "\n");
             System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " + serverIp);
-            System.exit(1);
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e) {
-                System.err.println("Error closing socket: " + e.getMessage());
-            }
         }
     }
 
     private void handleCommand(String command) {
         if (command.equals("exit")) {
-            System.out.println("Verbindung wird geschlossen...");
+            chatArea.append("Verbindung wird geschlossen...\n");
             System.exit(0);
         } else {
-            System.out.println("Unbekannter Befehl: " + command);
+            chatArea.append("Unbekannter Befehl: " + command + "\n");
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Geben Sie Ihren Benutzernamen ein:");
-        String username = reader.readLine();
-        System.out.println("Geben Sie die IP des Servers ein:");
-        String serverIp = reader.readLine();
-
-        ChatClient client = new ChatClient();
-        client.start(serverIp, 4444, username);
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ChatClient2 client = new ChatClient2();
+                String username = JOptionPane.showInputDialog("Geben Sie Ihren Benutzernamen ein:");
+                String serverIp = JOptionPane.showInputDialog("Geben Sie die IP des Servers ein:");
+                client.start(serverIp, 4444, username);
+            }
+        });
     }
 }
-
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module aufgabe16.aufgabenblatt16 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens aufgabe16.aufgabenblatt16 to javafx.fxml;
